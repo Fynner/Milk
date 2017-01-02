52e9c3794dc7fe6ef4c82aace46f5b13a495d167
Fynner <f625010909@qq.com>
2017-01-03 01:10:17 +0800

    Fynner

---
+++ app/src/main/java/com/xiaozhi/frame/local/manage/LocalDataManage.java
@@ -0,0 +1,13 @@
+package com.xiaozhi.frame.local.manage;
+
+/**
+ * Created by Fynner on 2017/1/2.
+ */
+
+public class LocalDataManage {
+    public static final String GOODS_DATA = "goods_data";
+
+
+
+
+}
